c871dbfefee7768540fcc147384c46cd1b6ce81c
Wats0ns <quentin.maire@protonmail.ch>
2018-01-31 19:00:46 +0100

    from twython to tweepy, cause twython doesn't seems to handle the 140->280 chars changements

--- edtech/edtech.py
+++ edtech/edtech.py
@@ -1,34 +1,44 @@
 from twython import Twython # conda install twython on a console
 import sys
+import tweepy
 sys.path.append('./edtech')
 
 class EdTech(object):
     def __init__(self, keyword, config):
         self.keyword = keyword
         self.config = config
-        self.twitter = Twython(config["consumer_key"],config["consumer_secret"],config["access_token"],config["access_token_secret"])
+        # self.twitter = Twython(config.twitter["consumer_key"],config.twitter["consumer_secret"],config.twitter["access_token"],config.twitter["access_token_secret"])
+        auth = tweepy.OAuthHandler(config.twitter["consumer_key"], config.twitter["consumer_secret"])
+
+        auth.set_access_token(config.twitter['access_token'], config.twitter['access_token_secret'])
+        self.twitter = tweepy.API(auth)
 
     def get_hastags(self, tweet):
         return [hashtag for hashtag in tweet.split() if hashtag[0] == '#']
 
     def run(self):
-        tweets = self.twitter.search(q=self.keyword, count=5)["statuses"]
+        tweets = self.twitter.search(q="macron", count=1, tweet_mode="extended", extended_tweet="full_text")
+        print (tweets[0])
+        print (dir(tweets[0]))
+        # print (tweets)
+        # print (tweets[0].keys())
+        # sys.exit(0)
         count = 0
         for tweet in tweets:
-            tweet_id = tweet["id"] # tweet id
-            date = tweet["created_at"] # tweet date
-            user = tweet["user"]["screen_name"] # user's screen_name
-            bio = tweet["user"]["description"] # user's bio
-            text = tweet["text"] # text of the tweet
-            user_location = tweet['user']['location']
-            geo = tweet['geo']
-            retweet_count = tweet['retweet_count']
-            created_at = tweet['created_at']
-            coords = tweet['coordinates']
+            tweet_id = tweet.id # tweet id
+            date = tweet.created_at # tweet date
+            user = tweet.user.screen_name # user's screen_name
+            bio = tweet.user.description # user's bio
+            text = tweet.full_text # text of the tweet
+            user_location = tweet.user.location
+            geo = tweet.geo
+            retweet_count = tweet.retweet_count
+            created_at = tweet.created_at
+            coords = tweet.coordinates
             print(coords, geo, user_location)
             count += 1
-            # print(tweet['hashtag'])
+            # print(tweet.hashtag)
             print("tweet id: ", tweet_id, "   sent on: ", date, "   tweeted by: @", user, sep="")
             print(text)
-        # print(tweets[0].keys())
+            print(tweet.truncated)
         print(count, " tweets were crawled")
