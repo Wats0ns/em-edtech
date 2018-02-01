950252ffc026ad1f10a8529df7a3ab9a31640d4d
Wats0ns <quentin.maire@protonmail.ch>
2018-02-01 19:31:36 +0100

    WIP

--- edtech/edtech.py
+++ edtech/edtech.py
@@ -1,5 +1,6 @@
-from twython import Twython # conda install twython on a console
+import pymongo
 import sys
+import json
 import tweepy
 sys.path.append('./edtech')
 
@@ -12,14 +13,21 @@ class EdTech(object):
 
         auth.set_access_token(config.twitter['access_token'], config.twitter['access_token_secret'])
         self.twitter = tweepy.API(auth)
+        self.mongo = pymongo.MongoClient(config.mongo['host'],
+                                username=config.mongo['username'],
+                                password=config.mongo['password'])
+        self.db = self.mongo.edtech
+
 
     def get_hastags(self, tweet):
         return [hashtag for hashtag in tweet.split() if hashtag[0] == '#']
 
+    def get_max_id(self):
+        return self.db.tweets.find_one(sort=[("id", pymongo.DESCENDING)])['id']
+
     def run(self):
-        tweets = self.twitter.search(q="macron", count=1, tweet_mode="extended", extended_tweet="full_text")
-        print (tweets[0])
-        print (dir(tweets[0]))
+        max_id = self.get_max_id()
+        tweets = self.twitter.search(q="edtech", count=100, tweet_mode="extended", extended_tweet="full_text", since_id=max_id)
         # print (tweets)
         # print (tweets[0].keys())
         # sys.exit(0)
@@ -35,10 +43,16 @@ class EdTech(object):
             retweet_count = tweet.retweet_count
             created_at = tweet.created_at
             coords = tweet.coordinates
-            print(coords, geo, user_location)
+            # print(coords, geo, user_location)
             count += 1
+            with open('data.json', 'w') as outfile:
+                json.dump(tweet._json, outfile)
             # print(tweet.hashtag)
             print("tweet id: ", tweet_id, "   sent on: ", date, "   tweeted by: @", user, sep="")
             print(text)
-            print(tweet.truncated)
+            # print(tweet.entities)
+            print(type(tweet._json))
+            target = tweet._json
+            target["easy_access_hastags"] = [entity["text"] for entity in tweet.entities["hashtags"]]
+            self.db.tweets.insert_one(tweet._json)
         print(count, " tweets were crawled")
